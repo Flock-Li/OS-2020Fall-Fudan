297e445291dc490ec4bef7089f6ff5ff90bf847e
Flock-Li <863813609@qq.com>
2020-10-19 15:04:37 +0800

    implement lab2 firstly

--- kern/main.c
+++ kern/main.c
@@ -21,5 +21,14 @@ main()
     alloc_init();
     cprintf("Allocator: Init success.\n");
     check_free_list();
+    void *p = kalloc();
+    void *pgdir = kalloc();
+    memset(pgdir,0,PGSIZE);
+    map_region(pgdir, 0, PGSIZE, V2P(p), 0);
+    memset(p,0xAC,PGSIZE);
+    load_sp_el1(pgdir);
+    for (int i =0; i < PGSIZE; i++){
+        print(*((int *)i) == "0xAC");
+    }
     while (1) ;
 }
--- kern/vm.c
+++ kern/vm.c
@@ -25,6 +25,21 @@ static uint64_t *
 pgdir_walk(uint64_t *pgdir, const void *va, int64_t alloc)
 {
     /* TODO: Your code here. */
+    uint64_t *current_table = pgdir;
+    uint64_t *entry = NULL;
+    for (int i = 0; i < 4; i++){
+        offset = PTX(i, va);
+        entry = current_table + offset;
+        if(!(*entry & PTE_P)){
+            if(alloc == 0)
+                return NULL;
+            char *page = kalloc();
+            memset(page, 0, PGSIZE);
+            *entry = V2P(*page) | PTE_P | PTE_USER | PTE_RW;
+        }
+        current_table =(uint64_t *) P2V(PTE_ADDR(*entry));
+    }
+    return entry;
 }
 
 /*
@@ -40,7 +55,23 @@ static int
 map_region(uint64_t *pgdir, void *va, uint64_t size, uint64_t pa, int64_t perm)
 {
     /* TODO: Your code here. */
+    uint64_t va_start = ROUNDDOWN(*va);
+    uint64_t va_end = ROUNDDOWN(*va + size -1);
+    uint64_t *pte;
+    for(;;){
+        if( (pte = pgdir_walk(pgdir, va, 1)) == 0 )
+            return -1;
+        if( *pte & PTE_P )
+            panic("remap");
+        *pte = P2V(pa) | perm | PTE_P | PTE_TABLE | PTE_AF;
+        if(va_start == va_end)
+            break;
+        va_start += PGSIZE;
+        pa += PGSIZE;
+    }
+    return 0;
 }
+
 
 /* 
  * Free a page table.
@@ -52,4 +83,12 @@ void
 vm_free(uint64_t *pgdir, int level)
 {
     /* TODO: Your code here. */
+    for(int i = 0; i < 512; i++){
+        uint64_t pte = pgdir[i];
+        if( pte & PTE_P ){
+            uint64_t *child = (uint64_t *) P2V(PTE_ADDR(*entry));
+            vm_free(child,level+1);
+        }
+    }
+    kfree(pgdir);
 }
