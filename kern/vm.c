45f2c740e745225d8221651240a31717b3accda0
Flock-Li <863813609@qq.com>
2020-10-29 19:10:23 +0800

    commit lab3

--- kern/vm.c
+++ kern/vm.c
@@ -24,7 +24,7 @@ extern uint64_t *kpgdir;
  *     a pointer into the new page table page.
  */
 
-static uint64_t *
+uint64_t *
 pgdir_walk(uint64_t *pgdir, const void *va, int64_t alloc)
 {
     uint64_t *current_table = pgdir;
@@ -54,7 +54,7 @@ pgdir_walk(uint64_t *pgdir, const void *va, int64_t alloc)
  * Hint: call pgdir_walk to get the corresponding page table entry
  */
 
-static int
+int
 map_region(uint64_t *pgdir, void *va, uint64_t size, uint64_t pa, int64_t perm)
 {
     /* TODO: Your code here. */
