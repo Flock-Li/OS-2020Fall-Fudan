d4b1b44c4626c2e532a1b415fd92cb3d66292c53
Flock-Li <863813609@qq.com>
2020-10-29 15:04:47 +0800

    modify Lab3

--- inc/trap.h
+++ inc/trap.h
@@ -5,6 +5,10 @@
 
 struct trapframe {
     /* TODO: Design your own trapframe layout here. */
+    uint64_t SPSR;
+    uint64_t SP;
+    uint64_t ESR;
+    uint64_t REGs[31];
 };
 
 void trap(struct trapframe *);
