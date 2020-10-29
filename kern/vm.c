db644b48fae0d3edc2d62d75b5f3f621a0d8e0c7
Flock-Li <863813609@qq.com>
2020-10-29 18:50:51 +0800

    finish lab3

--- kern/vm.c
+++ kern/vm.c
@@ -27,6 +27,21 @@ extern uint64_t *kpgdir;
 static uint64_t *
 pgdir_walk(uint64_t *pgdir, const void *va, int64_t alloc)
 {
+    uint64_t *current_table = pgdir;
+    uint64_t *entry = NULL;
+    for (int i = 0; i < 4; i++){
+        uint64_t offset = PTX(i, va);
+        entry = current_table + offset;
+        if(!(*entry & PTE_P)){
+            if(alloc == 0)
+                return NULL;
+            char *page = kalloc();
+            memset(page, 0, PGSIZE);
+            *entry = V2P(page) |  PTE_USER | PTE_RW;
+        }
+        current_table =(uint64_t *) P2V(PTE_ADDR(*entry));
+    }
+    return entry;
     /* TODO: Your code here. */
 }
 
@@ -43,6 +58,22 @@ static int
 map_region(uint64_t *pgdir, void *va, uint64_t size, uint64_t pa, int64_t perm)
 {
     /* TODO: Your code here. */
+    uint64_t va_start = ROUNDDOWN((char *)va,PGSIZE);
+    uint64_t va_end = ROUNDDOWN((char *)va + size - 1,PGSIZE);
+    uint64_t pa_start = ROUNDDOWN(pa,PGSIZE);
+    uint64_t *pte;
+    for(;;){
+        if( (pte = pgdir_walk(pgdir, (void *)va_start, 1)) == 0 )
+            return -1;
+        if( *pte & PTE_P )
+            panic("remap");
+        *pte = pa_start | perm | PTE_P | PTE_TABLE | PTE_AF;
+        if(va_start == va_end)
+            break;
+        va_start += PGSIZE;
+        pa_start += PGSIZE;
+    }
+    return 0;
 }
 
 /* 
@@ -54,6 +85,14 @@ map_region(uint64_t *pgdir, void *va, uint64_t size, uint64_t pa, int64_t perm)
 void
 vm_free(uint64_t *pgdir, int level)
 {
+    for(int i = 0; i < 512; i++){
+        uint64_t pte = pgdir[i];
+        if( pte & PTE_P ){
+            uint64_t *child = (uint64_t *) P2V(PTE_ADDR(pte));
+            vm_free(child,level+1);
+        }
+    }
+    kfree((char*)pgdir);
     /* TODO: Your code here. */
 }
 
